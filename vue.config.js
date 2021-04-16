439a1cecbc6adafec27e971c66d27253f0b17172
徐杰辉 <>
2021-04-16 16:37:22 +0800

    logo

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
         directories: {},
         win: {
           //win相关配置
-          icon: './public/favicon.ico',
+          icon: './build/icons/icon.ico',
           target: [
             {
               target: 'nsis', //利用nsis制作安装程序
@@ -34,9 +34,9 @@ module.exports = {
           oneClick: false, // 是否一键安装
           allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
           allowToChangeInstallationDirectory: true, // 允许修改安装目录
-          installerIcon: './public/favicon.ico', // 安装图标
-          uninstallerIcon: './public/favicon.ico', //卸载图标
-          installerHeaderIcon: './public/favicon.ico', // 安装时头部图标
+          installerIcon: './build/icons/icon.ico', // 安装图标
+          uninstallerIcon: './build/icons/icon.ico', //卸载图标
+          installerHeaderIcon: './build/icons/icon.ico', // 安装时头部图标
           createDesktopShortcut: true, // 创建桌面图标
           createStartMenuShortcut: true, // 创建开始菜单图标
           shortcutName: 'example' // 图标名称
